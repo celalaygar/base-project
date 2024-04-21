3af928c8ae87fd7372f799adeebedbd04bb105d7
hacı celal aygar <aygarcelal@gmail.com>
2024-04-21 21:49:36 +0300

    first commit

--- backend/app/src/main/java/com/project/app/filter/JwtAuthenticationFilter.java
+++ backend/app/src/main/java/com/project/app/filter/JwtAuthenticationFilter.java
@@ -87,7 +87,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
 
-
-        filterChain.doFilter(request, response);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, isrefreshtoken, content-type, xsrf-token");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
